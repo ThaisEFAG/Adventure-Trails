f7a53d73b92e7a2e02d5dc6b2e7214cf0fc1352d
Thaís Gonçalves <thaisefag1992@gmail.com>
2024-04-04 14:24:54 -0300

    divs card

--- adventure-trails/src/CardTrail.jsx
+++ adventure-trails/src/CardTrail.jsx
@@ -2,8 +2,31 @@
 
 const CardTrail = () => {
     return (
-        <div>
-        
+        <div className="card">
+            <div className="img">
+                <figure className="photo">
+
+                </figure>
+            </div>
+            <div className="content">
+                <div className="title-one">
+
+                </div>
+                <div className="icon">
+                    <span className="favorite-img-icon"></span>
+                </div>
+                <div className="record-name">
+
+                </div>
+                <div className="info">
+                    <span id="trail-duration"></span>
+                    <span id="trail-path"></span>
+                </div>
+                <div className="bottom-row">
+
+                </div>
+
+            </div>
         </div>
     )
 }

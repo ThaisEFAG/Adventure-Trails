4e0b5fa413d811edb66e81e8b13799fa8bbe7794
Thaís Gonçalves <thaisefag1992@gmail.com>
2024-04-18 17:11:31 -0300

    hook

---
+++ adventure-trails/src/Hooks/useFetch.jsx
@@ -0,0 +1,15 @@
+import {useState} from "react"
+function useFetch(url){
+
+    //o array como parametro do use state indica um item do json, no caso um obj
+    const [trilhas, setTrilhas] = useState({})
+
+    fetch(url)
+    // conversão dos dados para o padrão json 
+    .then((response) => response.json())
+    //o retorno da conversão cai no segundo then
+    .then((trilha) => setTrilhas(trilha))
+    .catch()
+}
+
+export default useFetch
